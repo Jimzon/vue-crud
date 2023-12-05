23ce3804ee0517ae1037cf69abda1f851e6429b7
Jimzon <104047846+Jimzon@users.noreply.github.com>
2023-12-05 23:11:49 +0800

    navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,5 @@
 <template>
   <main>
     <h1>Jimzon Pogi</h1>
-
   </main>
 </template>
---
+++ src/views/Students/View.vue
@@ -0,0 +1,6 @@
+<template>
+    <main>
+      <h1>This is the students page</h1>
+    </main>
+  </template>
+  
